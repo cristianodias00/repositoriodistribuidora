5060b44f558c5639f41fd9e13490fe987e266bee
Cristiano Dias <cristianodias00@hotmail.com>
2020-09-15 14:13:46 -0300

    Criação das Entidades Funcionário e Produto

--- DistribuidoraConsole/Funcionario.cs
+++ DistribuidoraConsole/Funcionario.cs
@@ -1,33 +1,32 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DistribuidoraConsole
 {
-    class Cliente
+    class Funcionario
     {
-        //Aqui é um construtor (Este NÃO está sendo utilizado, está só de exemplo)
-        public Cliente(string nome, string cpf)
-        {
-            Nome = nome;
-            Cpf = cpf;
-        }
         //Aqui é um construtor (Este é oque está sendo utilizado)
-        public Cliente()
+        public Funcionario()
         {
             CriadoEm = DateTime.Now;
         }
 
         //Aqui são os atributos, propriedades
         public string Nome { get; set; }
+        public string Rg { get; set; }
         public string Cpf { get; set; }
+        public string DataNasc { get; set; }
+        public string Cargo { get; set; }
+        public string Salario { get; set; }
+      
 
         public DateTime CriadoEm { get; set; }
 
         //Aqui é realizado o sobreescrever o método
         public override string ToString()
         {
-            return $"Nome: {Nome} | CPF: {Cpf} | Criado em: {CriadoEm}";
-        }
+            return $"Nome: {Nome} | RG: {Rg} | CPF: {Cpf} | Data Nascimento: {DataNasc} | Cargo: {Cargo} | Salário: {Salario} |Criado em: {CriadoEm}";        }
     }
 }
--- DistribuidoraConsole/Produto.cs
+++ DistribuidoraConsole/Produto.cs
@@ -1,33 +1,31 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DistribuidoraConsole
 {
-    class Cliente
+    class Produto
     {
-        //Aqui é um construtor (Este NÃO está sendo utilizado, está só de exemplo)
-        public Cliente(string nome, string cpf)
-        {
-            Nome = nome;
-            Cpf = cpf;
-        }
         //Aqui é um construtor (Este é oque está sendo utilizado)
-        public Cliente()
+        public Produto()
         {
             CriadoEm = DateTime.Now;
         }
 
         //Aqui são os atributos, propriedades
+        public string Tipo { get; set; }
         public string Nome { get; set; }
-        public string Cpf { get; set; }
+        public string Marca { get; set; }
+        public string Volume { get; set; }
+        public string Validade { get; set; }
 
         public DateTime CriadoEm { get; set; }
 
         //Aqui é realizado o sobreescrever o método
         public override string ToString()
         {
-            return $"Nome: {Nome} | CPF: {Cpf} | Criado em: {CriadoEm}";
+            return $"Tipo: {Tipo} | Nome: {Nome} | Marca: {Marca} | Volume (ml): {Volume} | Data: {Validade} | Criado em: {CriadoEm}";
         }
     }
 }
